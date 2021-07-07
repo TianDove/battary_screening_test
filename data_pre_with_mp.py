7f22546cb6e345bcb636c493e7f53dbe5bd71a01
XiangWang <awoshi37@163.com>
2021-07-07 20:51:09 +0800

    20210707： 添加FileOrganizer.tray_worker 添加FileOrganizer.file_organize_work_in_tray 添加FileOrganizer.get_tray_to_process() 用于开启托盘级多进程处理。

--- data_pre_with_mp.py
+++ data_pre_with_mp.py
@@ -198,6 +198,28 @@ class FileOrganizer():
                 temp_batch_list.append(batch)
         return temp_batch_list
 
+    def get_tray_to_process(self) -> list:
+        """
+        static_df: pd.DataFrame, batch: str, tray: str, tray_path: str
+        """
+        batch_list = self.get_batch_to_process()
+        temp_tray_list = []
+        for batch in iter(batch_list):
+            curr_batch = batch
+            current_batch_tray_path = os.path.join(self.dynamic_data_path, batch)
+            tray_list = os.listdir(current_batch_tray_path)
+            current_batch_static_data = self.read_static_data(batch)
+            for curr_tray in iter(tray_list):
+                curr_tray_path = os.path.join(current_batch_tray_path, curr_tray)
+                temp_tray_worker_dic = {
+                    'static_df': current_batch_static_data,
+                    'batch': curr_batch,
+                    'tray': curr_tray,
+                    'tray_path': curr_tray_path
+                }
+                temp_tray_list.append(temp_tray_worker_dic)
+        return temp_tray_list
+
     def dynamic_para_extract_cell(self, cell_no: int, para_data: pd.DataFrame) -> pd.DataFrame:
         """"""
         temp_dic = {}
@@ -242,7 +264,29 @@ class FileOrganizer():
             ver_flag = True
         return ver_flag
 
-    def file_organize(self, is_write: bool = True):
+    def file_organize_work_in_tray(self, is_write: bool = True):
+        self.is_write = is_write
+        self.make_file_dir()
+        batch_tray_list = self.get_tray_to_process()
+
+        # output
+        in_comp_cells = []
+        err_para_cells = []
+
+        # start processing
+        start_time = time.time()
+        if self.is_multi_worker:
+            with mp.Pool(processes=mp.cpu_count() - 1) as pool:
+                in_comp_cells, err_para_cells = pool.map(self.tray_worker, batch_tray_list)
+            end_time = time.time()
+            self.log_file_organie(start_time, end_time, in_comp_cells, err_para_cells)
+        else:
+            print('Single Processing, Pleas Using :self.file_organize_work_in_batch(), '
+                  'with self.is_multi_worker = False.')
+            sys.exit(0)
+
+
+    def file_organize_work_in_batch(self, is_write: bool = True):
         """"""
         self.is_write = is_write
         self.make_file_dir()
@@ -252,8 +296,7 @@ class FileOrganizer():
         start_time = time.time()
         # use multiprocess or not
         if self.is_multi_worker:
-            tqdm.set_lock(mp.RLock())  # for managing output contention
-            with mp.Pool(processes=mp.cpu_count(), initializer=tqdm.set_lock, initargs=(tqdm.get_lock(),)) as pool:
+            with mp.Pool(processes=mp.cpu_count() - 1) as pool:
                 in_comp_cells, err_para_cells = pool.map(self.batch_worker, batch_list)
             end_time = time.time()
         else:
@@ -262,18 +305,24 @@ class FileOrganizer():
                 in_comp_cells += temp_in_comp_cells
                 err_para_cells += temp_err_para_cells
             end_time = time.time()
+        # log
+        self.log_file_organie(start_time, end_time, in_comp_cells, err_para_cells)
+        print('Logging Finished, All Done.')
+        sys.exit(0)
 
+    def log_file_organie(self, start, end, in_comp_cells, err_para_cells):
+        """"""
         # logging
         now_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         log_file_path = os.path.join(self.out_path, now_time + '_' + 'FileOrganizer_Logging.txt')
         assert os.path.exists(log_file_path)
-        consumed_time = end_time - start_time
+        consumed_time = end - start
         # res = {'Incomplete Cells': in_comp_cells, 'Verification Failed Cells': err_para_cells}
 
         seperator = '-' * 89
         file_operation.write_txt(log_file_path, seperator)
         text_data = '| Finish Date: {} | Write Flag: {} | Multiprocess Flag: {} | Time Consume: {} s |'.format(
-            now_time, is_write, self.is_multi_worker, consumed_time)
+            now_time, self.is_write, self.is_multi_worker, consumed_time)
         file_operation.write_txt(log_file_path, text_data)
         file_operation.write_txt(log_file_path, seperator)
 
@@ -293,9 +342,10 @@ class FileOrganizer():
             file_operation.write_txt(log_file_path, text_data)
         file_operation.write_txt(log_file_path, seperator)
 
-
     def batch_worker(self, batch: str) -> (list, list):
         """"""
+        if self.is_multi_worker:
+            print('# {} Processing Start, PID: {}'.format(batch, os.getpid()))
         err_para_cells_list = []
         in_compelet_cells_list = []
         batch_cells_dic = {}
@@ -308,7 +358,7 @@ class FileOrganizer():
 
         # progress tray_bar parameter setup
         tray_total = len(tray_list)
-        text = "#{}-Processing...".format(curr_batch)
+        text = "#{}-Processing..., PID: {}".format(curr_batch, os.getpid())
         with tqdm(total=tray_total, desc=text) as tray_bar:
             for tray in iter(tray_list):
                 tray_bar.update()
@@ -370,11 +420,92 @@ class FileOrganizer():
                                 for name in iter(paras_dic.keys()):
                                     paras_dic[name].to_excel(writer, sheet_name=name)
                         # write cell pickle file
-                        write_pickle_path = os.path.join(self.out_path + '\\organized_data\\pickle', curr_cell + '.pickle')
+                        write_pickle_path = os.path.join(self.out_path +
+                                                         '\\organized_data\\pickle', curr_cell + '.pickle')
                         if not os.path.exists(write_pickle_path):
                             file_operation.save_dic_as_pickle(write_pickle_path, paras_dic)
+        if self.is_multi_worker:
+            print('# {} Processing End, PID: {}'.format(batch, os.getpid()))
         return in_compelet_cells_list, err_para_cells_list
 
+    def tray_worker(self, tray_worker_dic: dict) -> (list, list):
+        """"""
+
+        curr_batch = tray_worker_dic['batch']
+        curr_tray = tray_worker_dic['tray']
+        current_batch_static_data = tray_worker_dic['static_df']
+        current_batch_tray_path = tray_worker_dic['tray_path']
+
+        if self.is_multi_worker:
+            print('# {} Processing Start, PID: {}'.format(curr_batch + '_' + curr_tray, os.getpid()))
+
+        err_para_cells_list = []
+        in_compelet_cells_list = []
+
+        tray_name = self.tray_name_adjust(curr_tray)
+        current_tray_static_data = current_batch_static_data[current_batch_static_data['Tray ID']
+                                                             == tray_name]
+        assert current_tray_static_data.shape[0] == self.num_of_cell_in_tray
+        current_batch_tray_params_path = current_batch_tray_path
+        params_list = os.listdir(current_batch_tray_params_path)
+        paras_tray_dic = {}
+        for para_tray in iter(params_list):
+            # read current batch-tray-parameter
+            para_name = self.parameter_name_adjust(para_tray)
+            current_para_tray_data = self.read_dynamic_para_tray(current_batch_tray_params_path, para_tray)
+            current_para_tray_data.iloc[0, 0] = curr_batch + '-' + para_tray
+            paras_tray_dic.update({f'{para_name}': current_para_tray_data})
+        cell_no = [k for k in range(1, self.num_of_cell_in_tray + 1)]
+
+        # progress cell_bar parameter setup
+        cell_total = len(cell_no)
+        text = "#{}-Processing..., PID: {}".format(curr_batch + '_' + curr_tray, os.getpid())
+        with tqdm(total=cell_total, desc=text) as cell_bar:
+            for cell in iter(cell_no):
+                cell_bar.update()
+                paras_dic = {}
+                err_para_list = []
+                curr_cell = curr_batch + '_' + curr_tray + '_' + str(cell)
+                # get cell's static data
+                static_df = current_tray_static_data.iloc[cell - 1, 1:].to_frame().astype('str', 1)
+                static_df_trans = pd.DataFrame(static_df.values.T,
+                                               index=static_df.columns,
+                                               columns=static_df.index)
+                static_df_cp = static_df_trans.copy()
+                paras_dic.update({'Static': static_df_cp})
+                if len(paras_tray_dic.keys()) < 5:
+                    in_compelet_cells_list.append(curr_cell)
+                for para in iter(paras_tray_dic.keys()):
+                    # verify, such as:   Charge #1          Other
+                    #                  End Voltage(mV)   Capacity(mAH)
+                    curr_para = para
+                    # para extract mode:time, voltage, current, capacity
+                    para_df = self.dynamic_para_extract_cell(cell, paras_tray_dic[para])
+                    para_ver_flag = self.data_verification(static_df_trans, para_df, para)
+                    if (not para_df.empty) and para_ver_flag:
+                        paras_dic.update({f'{para}': para_df.copy()})
+                        # update paras_dic to cell_dic
+                    elif not para_ver_flag:
+                        err_para_list.append(curr_cell + '_' + curr_para)
+                if err_para_list:
+                    err_para_cells_list.append(curr_cell)
+                # update cells_dic
+                # batch_cells_dic.update({f'{curr_cell}': paras_dic.copy()})
+                if self.is_write:
+                    write_xlsx_path = os.path.join(self.out_path + '\\organized_data\\xlsx', curr_cell + '.xlsx')
+                    if not os.path.exists(write_xlsx_path):
+                        with pd.ExcelWriter(write_xlsx_path) as writer:
+                            # print(f'Write Cell File: {self.curr_cell}.', end='')
+                            for name in iter(paras_dic.keys()):
+                                paras_dic[name].to_excel(writer, sheet_name=name)
+                    # write cell pickle file
+                    write_pickle_path = os.path.join(self.out_path +
+                                                     '\\organized_data\\pickle', curr_cell + '.pickle')
+                    if not os.path.exists(write_pickle_path):
+                        file_operation.save_dic_as_pickle(write_pickle_path, paras_dic)
+        if self.is_multi_worker:
+            print('# {} Processing End, PID: {}'.format(curr_batch + '_' + curr_tray, os.getpid()))
+        return in_compelet_cells_list, err_para_cells_list
 
 class DataProcessor():
     """"""
@@ -392,4 +523,4 @@ if __name__ == '__main__':
     input_data_path = '.\\data\\2600P-01'
     output_data_path = '.\\data\\2600P-01_DataSet'
     m_file_organizer = FileOrganizer(input_data_path, output_data_path, is_multi_worker=True)
-    m_file_organizer.file_organize(is_write=True)
+    m_file_organizer.file_organize_work_in_tray(is_write=False)
